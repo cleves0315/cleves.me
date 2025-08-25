fbc6316b03202d3bfc685402a5bb7865057157d6
cleves0315 <cleves0315@foxmail.com>
2025-08-25 14:31:20 +0800

    remove configuration files, styles, and content; clean up project structure

--- src/app/mdx-page/page.tsx
+++
@@ -1,8 +0,0 @@
-import { markdownToHtml } from '@/lib/markdownToHtml'
-import { getPostBySlug } from '@/utils'
-
-export default async function Page() {
-  const htmlContent = await markdownToHtml(getPostBySlug('welcome'))
-
-  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-}
--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs'
-import path from 'path'
-
-export function getPostBySlug(slug: string): string {
-  const filePath = path.join(process.cwd(), 'src', 'content', `${slug}.md`)
-  return fs.readFileSync(filePath, 'utf8')
-}
